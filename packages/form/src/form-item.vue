983fd9ba89f560c88badc67b34e0710e11810140
Aaike Van Roekeghem <aaikevr@gmail.com>
2019-06-26 08:29:39 +0700

    remove test labels from from item

--- packages/form/src/form-item.vue
+++ packages/form/src/form-item.vue
@@ -10,7 +10,7 @@
     sizeClass ? 'el-form-item--' + sizeClass : ''
   ]">
     <label :for="labelFor" class="el-form-item__label" :style="labelStyle" v-if="label || $slots.label">
-      <slot name="label">{{label + form.labelSuffix}} {{JSON.stringify(initialValue)}} - {{JSON.stringify(fieldValue)}}</slot>
+      <slot name="label">{{label + form.labelSuffix}}</slot>
     </label>
     <div class="el-form-item__content" :style="contentStyle">
       <slot></slot>
